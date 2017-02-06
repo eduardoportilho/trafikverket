897d81fbcd0ef8f6cd504370e9c73d978f5b38a6
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-02-06 07:20:24 +0100

    Request skeleton

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
+let env = require('./environment_config')
+let request = require('browser-request')
+let Promise = require('promise')
+
 function getDeparturesFrom (stationName) {
-  return []
+  return new Promise(function (resolve, reject) {
+    request({
+      method: 'POST',
+      url: env['url'],
+      body: 'body',
+      function (err, response, body) {
+        if (err) {
+          reject(err)
+        }
+        var bodyObj = JSON.parse(body)
+        resolve(bodyObj)
+      }
+    })
+  })
 }
 
 const mainExport = {
