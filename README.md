7d1d08cb9ddb64e0cbc3c7d0816969c3b2cc3194
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-02-06 07:19:35 +0100

    Environment config

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ A wrapper for the [Trafikverket API](http://api.trafikinfo.trafikverket.se/API/)
 let trafikverket = require('trafikverket')
 trafikverket.getDeparturesFrom('Flen')
 ```
+
+## FAQ
+
+* How do I set my API key?
+** `export TRAFIKVERKET_API_KEY=<your API key>`
---
+++ src/environment_config.js
@@ -0,0 +1,12 @@
+let production = {
+  apiKey: process.env.TRAFIKVERKET_API_KEY,
+  url: 'http://api.trafikinfo.trafikverket.se/v1/data.json'
+}
+
+let test = {
+  apiKey: 'trafikverketApiKey',
+  url: 'trafikverketApiUrl'
+}
+
+var isProduction = process.env.NODE_ENV === 'production'
+module.exports = isProduction ? production : test
