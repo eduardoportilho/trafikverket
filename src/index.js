38373b1667bf082bd947296783782068a72c38d0
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-02-09 07:22:37 +0100

    Add optional ’toStation’ parameter to getDepartures and map the result to an object.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import env from './environment-config.js'
 import request from 'request'
 import Promise from 'promise'
+import fs from 'fs';
 
-function getDeparturesFrom (stationName) {
+function getDepartures (fromStationId, toStationId) {
+  let xmlRequestFile = 'train-announcement-request.xml';
+  let optionalFilters = ''
+  if (toStationId) {
+    optionalFilters += '<EQ name="ToLocation.LocationName" value="' + toStationId + '"/>'
+  }
+  let xmlRequest = fs.readFileSync(xmlRequestFile)
+    .toString()
+    .replace('{apikey}', env.apiKey)
+    .replace('{fromStationId}', fromStationId)
+    .replace('{optionalFilters}', optionalFilters);
+
+console.log(xmlRequest)
   let requestOptions = {
     method: 'POST',
-    url: env['url'],
-    body: 'body'
+    url: env.url,
+    body: xmlRequest
   }
   return new Promise(function (resolve, reject) {
     request(requestOptions,
@@ -14,15 +27,25 @@ function getDeparturesFrom (stationName) {
         if (err) {
           reject(err)
         }
-        var bodyObj = JSON.parse(body)
-        resolve(bodyObj)
+        let bodyObj = JSON.parse(body)
+        let anouncements = bodyObj['RESPONSE']['RESULT'][0]['TrainAnnouncement'].map(function (anouncement) {
+          var datetime = anouncement['AdvertisedTimeAtLocation'].split('T')
+          return {
+            train: anouncement['AdvertisedTrainIdent'],
+            track: anouncement['TrackAtLocation'],
+            date: datetime[0],
+            time: datetime[1],
+            destination: anouncement['ToLocation']['LocationName']
+          }
+        })
+        resolve(anouncements)
       }
     )
   })
 }
 
 const mainExport = {
-  getDeparturesFrom: getDeparturesFrom
+  getDepartures: getDepartures
 }
 
 export default mainExport
