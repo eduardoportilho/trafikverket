12b300b6b6306939a5bb74f9a30ee6e5c85cd604
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-02-13 08:09:12 +0100

    Fix lint errors in environment-config.test.js

--- test/environment-config.test.js
+++ test/environment-config.test.js
@@ -1,8 +1,8 @@
 import {expect} from 'chai'
 import env from '../src/environment-config'
 
-describe('environment-config', function() {
-  it('should use test environment', function() {
+describe('environment-config', function () {
+  it('should use test environment', function () {
     expect(env.apiKey).to.equal('trafikverketApiKey')
     expect(env.url).to.equal('trafikverketApiUrl')
   })
